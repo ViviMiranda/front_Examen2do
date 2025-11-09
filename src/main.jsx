5552064e172c8e44b0cbc258685d057d5e1cfe8d
IDGS-803-22001758 <79677@alumnos.utleon.edu.mx>
2025-11-08 23:23:41 -0600

    cambio main

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,6 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./pages/index.jsx";
 import "./index.css";
 
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/sw.js")
-      .then(() => console.log("Service Worker registrado"))
-      .catch((err) => console.error("Error registrando SW:", err));
-  });
-}
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
